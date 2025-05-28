3b4c64ab73978f9a356d140172d9b57bd075a9c6
Haru-1210 <tech.h.121210@gmail.com>
2025-05-28 16:46:08 +0900

    20250528_必要なカラムを記述1

--- db/schema.rb
+++ db/schema.rb
@@ -12,8 +12,14 @@
 
 ActiveRecord::Schema[7.1].define(version: 2025_05_28_065709) do
   create_table "users", charset: "utf8mb3", force: :cascade do |t|
+    t.string "nickname", null: false
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
+    t.string "last_name", null: false
+    t.string "first_name", null: false
+    t.string "last_name_kana", null: false
+    t.string "first_name_kana", null: false
+    t.date "birthdate", null: false
     t.string "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"

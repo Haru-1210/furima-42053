0d13105e44bd93677998e7557e89ea9e45053e64
Haru-1210 <tech.h.121210@gmail.com>
2025-06-04 17:15:39 +0900

    20250604_モデルの単体テストを実装3

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,8 +1,13 @@
 FactoryBot.define do
   factory :user do
-    nickname              {Faker::Name.initials(number: 2)}
-    email                 {Faker::Internet.email}
-    password              {Faker::Internet.password(min_length: 6)}
-    password_confirmation {password}
+    nickname              { Faker::Name.initials(number: 2) }
+    email                 { Faker::Internet.email }
+    password              { 'abc123' }
+    password_confirmation { 'abc123' }
+    last_name             { '山田' }
+    first_name            { '太郎' }
+    last_name_kana        { 'ヤマダ' }
+    first_name_kana       { 'タロウ' }
+    birthdate             { '1990-01-01' }
   end
 end

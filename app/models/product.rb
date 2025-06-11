a85f0ba0f66eac79c52a6716260240c679d21809
Haru-1210 <tech.h.121210@gmail.com>
2025-06-11 21:55:33 +0900

    20250611_prefectureモデルの追加1

--- app/models/product.rb
+++ app/models/product.rb
@@ -6,10 +6,12 @@ class Product < ApplicationRecord
   belongs_to :category
   belongs_to :condition
   belongs_to :shipping_charge
+  belongs_to :prefecture
   
   validates :item_name, :description, :price, presence: true
 
   validates :category_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :condition_id, numericality: { other_than: 1, message: "can't be blank" }
   validates :shipping_charge_id, numericality: { other_than: 1, message: "can't be blank" }
+  validates :prefecture_id, numericality: { other_than: 1, message: "can't be blank" }
 end

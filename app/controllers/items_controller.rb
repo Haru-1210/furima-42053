83ba099e9a89a52fcc8125875c156577d5dc182e
Haru-1210 <tech.h.121210@gmail.com>
2025-06-13 18:31:56 +0900

    20250613_items_controller.rbとroutes.rbの編集1

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,6 @@
 class ItemsController < ApplicationController
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     @items = Item.all.order(created_at: :desc)
@@ -8,7 +10,7 @@ class ItemsController < ApplicationController
   end
 
   def new
-    @Item = Item.new
+    @item = Item.new
   end
 
   def create

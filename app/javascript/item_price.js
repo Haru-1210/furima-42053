5a191dbd2acb954c390ffb936ca96385425277ed
Haru-1210 <tech.h.121210@gmail.com>
2025-06-15 21:57:19 +0900

    20250615_item_price.jsの追加1

---
+++ app/javascript/item_price.js
@@ -0,0 +1,20 @@
+window.addEventListener('turbo:load', () => {
+  const priceInput = document.getElementById("item-price");
+  if (!priceInput) return; // 新規出品ページだけで動くように
+
+  priceInput.addEventListener("input", () => {
+    const inputValue = priceInput.value;
+
+    const addTaxDom = document.getElementById("add-tax-price");
+    const profitDom = document.getElementById("profit");
+
+    // 販売手数料 = 価格の10% (小数点切り捨て)
+    const fee = Math.floor(inputValue * 0.1);
+
+    // 販売利益 = 価格 - 手数料
+    const profit = inputValue - fee;
+
+    addTaxDom.innerHTML = fee;
+    profitDom.innerHTML = profit;
+  });
+});

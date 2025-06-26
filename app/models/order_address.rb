a3854133e1c1eab6aa0d9a80589fc1516a668005
Haru-1210 <tech.h.121210@gmail.com>
2025-06-26 23:51:01 +0900

    20250626_Formオブジェクトに購入情報の属性を追加

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -1,6 +1,6 @@
 class OrderAddress
   include ActiveModel::Model
-  attr_accessor :token, :post_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id
+  attr_accessor :token, :postal_code, :prefecture_id, :city, :house_number, :building_name, :phone_number, :user_id, :item_id
  
   with_options presence: true do
     validates :token

9b1893a86f6b022d9d64313d2519a157bc12ff18
Haru-1210 <tech.h.121210@gmail.com>
2025-06-26 21:05:51 +0900

    20250626_orders_controllerの重複記述と構文エラーを修正

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,12 +1,11 @@
 class OrdersController < ApplicationController
+  before_action :set_item
 
   def index
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new
   end
 
   def create
-    @item = Item.find(params[:item_id])
     @order_address = OrderAddress.new(order_address_params)
     if @order_address.valid?
       @order_address.save
@@ -18,9 +17,14 @@ class OrdersController < ApplicationController
 
   private
 
+  def set_item
+    @item = Item.find(params[:item_id])
+  end
+
   def order_address_params
     params.require(:order_address).permit(
       :postal_code, :prefecture_id, :city, :house_number,
       :building_name, :phone_number, :token
     ).merge(user_id: current_user.id, item_id: params[:item_id])
+  end
 end

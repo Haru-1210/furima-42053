3cf4daf87b7e631d16e32e136fbd17b449ba4c6d
Haru-1210 <tech.h.121210@gmail.com>
2025-06-26 17:13:23 +0900

    20250626_ルーティングを修正し、購入機能に必要なネストされたordersルートを追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,10 @@
 Rails.application.routes.draw do
-  get 'orders/index'
-  get 'orders/create'
   devise_for :users
   root to: "items#index"
-  resources :items
+
+  resources :items do
+    resources :orders, only: [:index, :create]
+  end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 

d0dccdefa3c4638341f6b577b063c26031eea165
Haru-1210 <tech.h.121210@gmail.com>
2025-06-27 14:10:30 +0900

    20250627_OrderAddressクラスのsaveメソッド内のpost_codeのタイポを修正

--- app/models/order_address.rb
+++ app/models/order_address.rb
@@ -16,7 +16,7 @@ class OrderAddress
   def save
     order = Order.create(user_id: user_id, item_id: item_id)
     Address.create(
-      post_code: post_code, prefecture_id: prefecture_id, city: city,
+      post_code: postal_code, prefecture_id: prefecture_id, city: city,
       house_number: house_number, building_name: building_name,
       phone_number: phone_number, order_id: order.id
     )
